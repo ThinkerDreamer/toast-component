247a38ebfe67139efbe4dc4a069ef437f527a8ec
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-13 20:48:13 +0100

    create ToastStackContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,16 @@ import React from 'react';
 import ToastPlayground from '../ToastPlayground';
 import Footer from '../Footer';
 
+export const ToastStackContext = React.createContext();
+
 function App() {
+  const [toastStack, setToastStack] = React.useState([]);
+
   return (
-    <>
+    <ToastStackContext.Provider value={{ toastStack, setToastStack }}>
       <ToastPlayground />
       <Footer />
-    </>
+    </ToastStackContext.Provider>
   );
 }
 
