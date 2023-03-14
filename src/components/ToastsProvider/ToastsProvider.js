5ddcca6d057803c1da48e0e9f87839b35cd243ac
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-14 14:55:36 +0100

    add useEffect: dismiss all toasts on Esc keypress

--- src/components/ToastsProvider/ToastsProvider.js
+++ src/components/ToastsProvider/ToastsProvider.js
@@ -5,6 +5,20 @@ export const ToastStackContext = React.createContext();
 function ToastsProvider({ children }) {
   const [toastStack, setToastStack] = React.useState([]);
 
+  React.useEffect(() => {
+    function dismissAllOnEsc(e) {
+      if (e.code === 'Escape') {
+        setToastStack([]);
+      }
+    }
+
+    window.addEventListener('keydown', dismissAllOnEsc);
+
+    return () => {
+      window.removeEventListener('keydown', dismissAllOnEsc);
+    };
+  }, []);
+
   function addToast({ variant, message }) {
     const id = crypto.randomUUID();
     const newToast = {
